c58f9874b870147929bf26d4abf95745d4419ec1
NILBOGtheSavior <naztechmedia@gmail.com>
2025-10-30 14:10:29 -0500

    Implemented dynamic light flicker

--- scripts/static_and_co.gd
+++ scripts/static_and_co.gd
@@ -1,30 +1,46 @@
 extends Node3D
-
-class LightUnit:
-	var material : StandardMaterial3D
-	var light : OmniLight3D
-	var timer : Timer
-	
-	func _init(t) -> void:
-		timer = t
-		timer.timeout.connect(_on_flicker)
-		timer.start(randf_range(5.0, 10.0))
-	
-	func _on_flicker():
-		material.emission_energy_multiplier = 0.0
-		light.light_energy = 0.0
+class_name StaticCo
 
 var light1 : LightUnit
 var light2 : LightUnit
 
 func _ready() -> void:
 	prepare_lights()
-	
+
 func prepare_lights():
-	light1 = LightUnit.new($Timer)
-	light1.material = $LightPanel.get_surface_override_material(1)
-	light1.light = $LightPanelOmni
+	light1 = LightUnit.new($LightPanel1/Timer)
+	light1.material = $LightPanel1/Mesh.get_surface_override_material(1)
+	light1.light = $LightPanel1
 	
-	light2 = LightUnit.new($Timer001)
-	light2.material = $LightPanel_001.get_surface_override_material(1)
-	light2.light = $LightPanel_001Omni
+	light2 = LightUnit.new($LightPanel2/Timer)
+	light2.material = $LightPanel2/Mesh.get_surface_override_material(1)
+	light2.light = $LightPanel2
+
+class LightUnit:
+	var material : StandardMaterial3D
+	var light : OmniLight3D
+		
+	var timer : Timer
+	var flicker_cycles : int
+	
+	func _init(t : Timer) -> void:
+		timer = t
+		timer.timeout.connect(flicker_cycle)
+		flicker()
+		
+	func flicker():
+		flicker_cycles = randi_range(5, 15)
+		timer.start(randf_range(1.0, 10.0))
+	
+	func flicker_cycle():
+		if flicker_cycles:
+			flicker_cycles -= 1
+			var energy = randf()
+			material.emission_energy_multiplier = energy * 5.0
+			light.light_energy = energy * 0.2
+			timer.start(randf_range(0.05, 0.1))
+		else:
+			material.emission_energy_multiplier = 5.0
+			light.light_energy = 0.2
+			flicker()
+			
