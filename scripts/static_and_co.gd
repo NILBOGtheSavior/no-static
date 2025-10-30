f22a27e7c912bd3b5bedbc977105a152a5b528ab
NILBOGtheSavior <naztechmedia@gmail.com>
2025-10-30 12:30:02 -0500

    Modified noise shader and removed outer lightsource

--- scripts/static_and_co.gd
+++ scripts/static_and_co.gd
@@ -1,8 +1,18 @@
 extends Node3D
 
 class LightUnit:
-	var emission : float
-	var energy : float
+	var material : StandardMaterial3D
+	var light : OmniLight3D
+	var timer : Timer
+	
+	func _init(t) -> void:
+		timer = t
+		timer.timeout.connect(_on_flicker)
+		timer.start(randf_range(5.0, 10.0))
+	
+	func _on_flicker():
+		material.emission_energy_multiplier = 0.0
+		light.light_energy = 0.0
 
 var light1 : LightUnit
 var light2 : LightUnit
@@ -11,10 +21,10 @@ func _ready() -> void:
 	prepare_lights()
 	
 func prepare_lights():
-	light1 = LightUnit.new()
-	light1.emission = $LightPanel.get_surface_override_material(1).emission_energy_multiplier
-	light1.energy = $LightPanelOmni.light_energy
+	light1 = LightUnit.new($Timer)
+	light1.material = $LightPanel.get_surface_override_material(1)
+	light1.light = $LightPanelOmni
 	
-	light2 = LightUnit.new()
-	light2.emission = $LightPanel_001.get_surface_override_material(1).emission_energy_multiplier
-	light2.energy = $LightPanel_001Omni.light_energy
+	light2 = LightUnit.new($Timer001)
+	light2.material = $LightPanel_001.get_surface_override_material(1)
+	light2.light = $LightPanel_001Omni
